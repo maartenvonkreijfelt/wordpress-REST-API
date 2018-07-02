4136eb1b487eb9bdee7328d710ab342b32414a42
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-07-02 11:42:47 +0200

    update

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -1,6 +1,9 @@
 <?php
-add_action( 'rest_api_init', 'tschild_register_fields' );
+/**
+ * Add fields to the REST API response
+ */
 
+add_action( 'rest_api_init', 'tschild_register_fields' );
 function tschild_register_fields() {
     register_rest_field( 'post',
         'previous_post_ID',
@@ -10,9 +13,6 @@ function tschild_register_fields() {
             'schema'          => null,
         )
     );
-
-
-
     register_rest_field( 'post',
         'previous_post_title',
         array(
@@ -21,9 +21,6 @@ function tschild_register_fields() {
             'schema'          => null,
         )
     );
-
-
-
     register_rest_field( 'post',
         'previous_post_link',
         array(
@@ -34,15 +31,25 @@ function tschild_register_fields() {
     );
 }
 
-/**
- * Add fields to the REST API response
- */
 function tschild_get_previous_post_ID() {
     return get_previous_post()->ID;
 }
+
 function tschild_get_previous_post_title() {
     return get_previous_post()->post_title;
 }
+
 function tschild_get_previous_post_link() {
     return get_permalink( get_previous_post()->ID );
+}
+
+/**
+ * Enqueue JavaScript.
+ */
+
+add_action( 'wp_enqueue_scripts', 'tsc_scripts' );
+function tsc_scripts() {
+    if ( is_single() ){
+        wp_enqueue_script( 'tsc-js', get_theme_file_uri('JS/previous.ajax.js'), array('jquery'), '0.1', true );
+    }
 }
